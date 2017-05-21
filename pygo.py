313ff92db25577ce7a398b2aceecfd1d08b1e5b1
Vibius Vibidius Zosimus <mudsharkmk@gmail.com>
2017-05-21 17:16:30 +0200

    added hover; python3 now

--- pygo.py
+++ pygo.py
@@ -1,4 +1,4 @@
-from Tkinter import *
+from tkinter import *
 
 class Pygo(object):
     def __init__(self):
@@ -6,9 +6,11 @@ class Pygo(object):
         self.cell_size = self.width / 20.0
         self.r = self.cell_size / 3.0
         self.delay = 5
-        self.pieces = [[None] * 19 for _ in xrange(19)]
+        self.pieces = [[None] * 19 for _ in range(19)]
         self.curr_num = 0
-        self.scheme = 0
+        self.scheme = 1
+        self.colInd = -1
+        self.rowInd = -1
 
         self.root = Tk()
         self.canvas = Canvas(self.root, width=self.width, height=self.height)
@@ -16,6 +18,7 @@ class Pygo(object):
         self.root.bind('<Button-1>', self.left_pressed)
         self.root.bind('<Button-2>', self.right_pressed)
         self.root.bind('<Key>', self.key)
+        self.root.bind('<Motion>', self.motion)
 
     def flip_curr_num(self):
         if self.curr_num == 0:
@@ -26,7 +29,7 @@ class Pygo(object):
     def left_pressed(self, event):
         c = int(event.x / self.cell_size - .5)
         r = int(event.y / self.cell_size - .5)
-        if r not in xrange(19) or c not in xrange(19): return
+        if r not in range(19) or c not in range(19): return
         if self.pieces[r][c] == None:
             self.pieces[r][c] = self.curr_num
             self.flip_curr_num()
@@ -34,7 +37,7 @@ class Pygo(object):
     def right_pressed(self, event):
         c = int(event.x / self.cell_size - .5)
         r = int(event.y / self.cell_size - .5)
-        if r not in xrange(19) or c not in xrange(19): return
+        if r not in range(19) or c not in range(19): return
         self.pieces[r][c] = None
 
     def key(self, event):
@@ -45,14 +48,18 @@ class Pygo(object):
         elif event.keysym == 'w':
             self.scheme = 1
         elif event.keysym == 'c':
-            self.pieces = [[None] * 19 for _ in xrange(19)]
+            self.pieces = [[None] * 19 for _ in range(19)]
+
+    def motion(self, event):
+        self.colInd = int(event.x / self.cell_size - .5)
+        self.rowInd = int(event.y / self.cell_size - .5)
 
     def draw_board(self):
         if self.scheme == 0:
             fill = 'white'
         elif self.scheme == 1:
             fill = 'black'
-        for i in xrange(19):
+        for i in range(19):
             self.canvas.create_line(self.cell_size, self.cell_size * (i + 1), self.width - self.cell_size, self.cell_size * (i + 1), fill=fill)
             self.canvas.create_line(self.cell_size * (i + 1), self.cell_size, self.cell_size * (i + 1), self.height - self.cell_size, fill=fill)
         for x in [0, 2, 4]:
@@ -64,9 +71,9 @@ class Pygo(object):
             outline = 'white'
         elif self.scheme == 1:
             outline = 'black'
-        for r in xrange(len(self.pieces)):
+        for r in range(len(self.pieces)):
             row = self.pieces[r]
-            for c in xrange(len(row)):
+            for c in range(len(row)):
                 if row[c] == None:
                     continue
                 elif row[c] == 1:
@@ -86,6 +93,20 @@ class Pygo(object):
             outline = 'black'
         self.canvas.create_oval(10, 10, self.r * 2 + 10, self.r * 2 + 10, fill=fill, outline=outline)
 
+    def draw_hover(self):
+        if self.colInd not in range(19) or self.rowInd not in range(19): return
+        if self.scheme == 0:
+            outline = 'white'
+        elif self.scheme == 1:
+            outline = 'black'
+        if self.pieces[self.rowInd][self.colInd] != None:
+            outline = 'red'
+        if self.curr_num == 0:
+            fill = 'black'
+        elif self.curr_num == 1:
+            fill = 'white'
+        self.canvas.create_oval(self.cell_size * (self.colInd + 1) - self.r, self.cell_size * (self.rowInd + 1) - self.r, self.cell_size * (self.colInd + 1) + self.r, self.cell_size * (self.rowInd + 1) + self.r, outline=outline, fill=fill)
+
     def redraw(self):
         self.canvas.delete(ALL)
         if self.scheme == 0:
@@ -97,6 +118,7 @@ class Pygo(object):
         self.draw_board()
         self.draw_pieces()
         self.draw_curr_piece()
+        self.draw_hover()
 
         self.canvas.update()
 
